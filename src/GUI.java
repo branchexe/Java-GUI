c164383e9084acc37274f88623b8c28066586a34
CactusAndAda <120757670+CactusAndAda@users.noreply.github.com>
2023-01-21 18:44:58 -0600

    Initial commit

---
+++ src/GUI.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+
+    int counts = 0;
+    JLabel label;
+
+    public GUI() {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Click Me!");
+        button.addActionListener(this);
+
+        label = new JLabel("Number of clicks: 0");
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("GUI");
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        counts++;
+        label.setText("Number of Clicks: " +  counts);
+    }
+
+}
